11e38ff8bc6bd80d49a36c1a6118027bae24dbaa
mutaliksamarth <mutaliksamarth.02@gmail.com>
2024-10-16 01:28:04 +0530

    Backend Completed

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from 'hono'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+
+
+
+const app = new Hono()
+
+app.post('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+
+app.route("/api/v1/user",userRouter );
+app.route("/api/v1/blog", blogRouter);
+
+
+export default app
